0dca786bce269ab2e47f0fffde8bd1fb2ed5ddd4
kamel <gkamel2012@gmail.com>
2018-07-12 18:39:51 +0200

    nouvelle version

--- myappWeb/src/main/java/fr/afcepf/ai103/service/ServiceCompte.java
+++ myappWeb/src/main/java/fr/afcepf/ai103/service/ServiceCompte.java
@@ -1,11 +1,13 @@
 package fr.afcepf.ai103.service;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import fr.afcepf.ai103.data.Compte;
+import fr.afcepf.ai103.data.Operation;
 
 public class ServiceCompte {
 
@@ -28,11 +30,24 @@ public class ServiceCompte {
 	public List<Compte> comptesDuClient(Long numClient) {
 
 		List<Compte> listeComptes = new ArrayList<Compte>();
-
+		// simulation de valeurs récupérées en base:
 		listeComptes.add(mapComptes.get(123456L));
 		listeComptes.add(mapComptes.get(987654L));
 
 		return listeComptes;
+	}
+
+	public List<Operation> oprationDuCompte(long numCompte) {
+
+		List<Operation> listeOperations = new ArrayList<Operation>();
+		if (numCompte % 2 == 0) {
+			listeOperations.add(new Operation(1L, new Date(), -30.0, "achat livre"));
+			listeOperations.add(new Operation(2L, new Date(), -10.0, "achat DVD"));
+		} else {
+			listeOperations.add(new Operation(3L, new Date(), -15.0, "achat Mag"));
+		}
+
+		return listeOperations;
 	}
 
 	public void transferer(double montant, long numCptDebiteur, long numCptCrediteur) {
--- myappWeb/src/main/java/fr/afcepf/ai103/web/CompteBean.java
+++ myappWeb/src/main/java/fr/afcepf/ai103/web/CompteBean.java
@@ -1,12 +1,15 @@
 package fr.afcepf.ai103.web;
 
 import fr.afcepf.ai103.data.Compte;
+import fr.afcepf.ai103.data.Operation;
 import fr.afcepf.ai103.service.ServiceCompte;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.event.ActionEvent;
 import javax.faces.event.ComponentSystemEvent;
 
 @ManagedBean
@@ -14,17 +17,38 @@ import javax.faces.event.ComponentSystemEvent;
 
 public class CompteBean {
 
-	private Long numClient = null;
 	private ServiceCompte servciceCompte = new ServiceCompte();
-	private List<Compte> comptes;
+
+	private Long numClient = null;
+	private Long numCptDebiteur = null;
+	private Long numCptCrediteur = null;
+	private Double montant = null;
+
+	private List<Compte> comptes = null;
+	private List<Operation> operations = null;
+
+	private Long selectedNumCompte = null;
 
 	public CompteBean() {
+		operations = new ArrayList<Operation>();
+	}
+
+	public Long getSelectedNumCompte() {
+		return selectedNumCompte;
+	}
+
+	public void setSelectedNumCompte(Long selectedNumCompte) {
+		this.selectedNumCompte = selectedNumCompte;
 	}
 
 	// Méthode appelée après que le numClient soit automatiquement mis à jour par
 	// JSF
 	public void initComptes(ComponentSystemEvent event) {
 		comptes = servciceCompte.comptesDuClient(numClient);
+	}
+
+	public void onSelectCompte(ActionEvent event) {
+		operations = servciceCompte.oprationDuCompte(numClient);
 	}
 
 	public Long getNumClient() {
@@ -51,10 +75,6 @@ public class CompteBean {
 		this.comptes = comptes;
 	}
 
-	private Long numCptDebiteur = null;
-	private Long numCptCrediteur = null;
-	private Double montant = null;
-
 	public Long getNumCptDebiteur() {
 		return numCptDebiteur;
 	}
@@ -77,6 +97,14 @@ public class CompteBean {
 
 	public void setMontant(Double montant) {
 		this.montant = montant;
+	}
+
+	public List<Operation> getOperations() {
+		return operations;
+	}
+
+	public void setOperations(List<Operation> operations) {
+		this.operations = operations;
 	}
 
 	public String effectuerVirement() {
