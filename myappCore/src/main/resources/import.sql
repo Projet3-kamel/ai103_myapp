4f0e978cb7cb9bbe3049a66c33bf247c53c7566d
kamel <gkamel2012@gmail.com>
2018-07-16 10:11:43 +0200

    commit projet myappCore

--- myappCore/src/main/resources/import.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO Client(numClient, prenom, nom, email, adresse, telephone, password) VALUES (1, 'Jean', 'DU JARDIN', 'jean.dujardin@gmail.com', '1 rue Thénard 75005 Paris', '06552233', 'pwd1')
-INSERT INTO Client(numClient, prenom, nom, email, adresse, telephone, password) VALUES (2, 'axelle', 'Aire', 'axelle.aire@gmail.com', '1 rue elle 69005 Lyon', '06552233', 'pwd2')
-INSERT INTO Client(numClient, prenom, nom, email, adresse, telephone, password) VALUES (3, 'amar', 'DEKABYLIE', 'amardekabylie@gmail.com', '1 rue Thénard 94120 Créteil', '06552233', 'pwd3')
-INSERT INTO Client(numClient, prenom, nom, email, adresse, telephone, password) VALUES (4, 'air', 'Nike', 'air.nike@gmail.com', '23 rue de Londre 75009 PAris', '06552233', 'pwd4')
